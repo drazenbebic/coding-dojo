d21d71cee003f077f17e1b51f8d75e051dd5a188
Drazen Bebic <drazen.bebic@outlook.com>
2025-03-07 12:16:31 +0100

    feat: initial commit

---
+++ packages/ui/webpack.config.ts
@@ -0,0 +1,45 @@
+import type webpack from 'webpack';
+import path from 'path';
+
+const config: webpack.Configuration = {
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x)?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.svg/,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+};
+
+module.exports = config;
